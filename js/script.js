74485b5205d28b2906e37a541d866ab7a467a9e8
Tatiana Velasquez <tative23992@gmail.com>
2023-03-10 15:25:58 -0300

    Primer commit

--- js/script.js
+++ js/script.js
@@ -40,6 +40,38 @@ function updCursoPython() {
     document.getElementById("cursoPython").innerHTML=document.getElementById("inputCursoPython").value;
 }
 
+function  showInput5() {
+    document.getElementById("inputAptitudes1").style.display="block";
+}
+
+function updAptitudes1() {
+    document.getElementById("aptitudes1").innerHTML=document.getElementById("inputAptitudes1").value;
+}
+
+function  showInput6() {
+    document.getElementById("inputAptitudes2").style.display="block";
+}
+
+function updAptitudes2() {
+    document.getElementById("aptitudes2").innerHTML=document.getElementById("inputAptitudes2").value;
+}
+
+function  showInput7() {
+    document.getElementById("inputProyecto1").style.display="block";
+}
+
+function updProyecto1() {
+    document.getElementById("proyecto1").innerHTML=document.getElementById("inputProyecto1").value;
+}
+
+function  showInput8() {
+    document.getElementById("inputProyecto2").style.display="block";
+}
+
+function updProyecto2() {
+    document.getElementById("proyecto2").innerHTML=document.getElementById("inputProyecto2").value;
+}
+
 const exampleModal = document.getElementById('exampleModal')
 exampleModal.addEventListener('show.bs.modal', event => {
   // Button that triggered the modal
@@ -115,6 +147,70 @@ function  eliminarItem3(idItem) {
     console.log(document.getElementById(idItem))
     document.getElementById(idItem).className="d-none"
     const myModal = new bootstrap.Modal(document.getElementById('exampleModal3'))
+
+
+}
+
+const exampleModal4 = document.getElementById('exampleModal4')
+exampleModal4.addEventListener('show.bs.modal', event => {
+  const button = event.relatedTarget
+  const recipient = button.getAttribute('data-bs-whatever')
+  const modalBodyInput = exampleModal4.querySelector('.modal-body input')
+  modalBodyInput.value = recipient
+})
+function  eliminarItem4(idItem) {
+    console.log(idItem)
+    console.log(document.getElementById(idItem))
+    document.getElementById(idItem).className="d-none"
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModal4'))
+
+
+}
+
+const exampleModal5 = document.getElementById('exampleModal5')
+exampleModal5.addEventListener('show.bs.modal', event => {
+  const button = event.relatedTarget
+  const recipient = button.getAttribute('data-bs-whatever')
+  const modalBodyInput = exampleModal5.querySelector('.modal-body input')
+  modalBodyInput.value = recipient
+})
+function  eliminarItem5(idItem) {
+    console.log(idItem)
+    console.log(document.getElementById(idItem))
+    document.getElementById(idItem).className="d-none"
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModal5'))
+
+
+}
+
+const exampleModal6 = document.getElementById('exampleModal6')
+exampleModal6.addEventListener('show.bs.modal', event => {
+  const button = event.relatedTarget
+  const recipient = button.getAttribute('data-bs-whatever')
+  const modalBodyInput = exampleModal6.querySelector('.modal-body input')
+  modalBodyInput.value = recipient
+})
+function  eliminarItem6(idItem) {
+    console.log(idItem)
+    console.log(document.getElementById(idItem))
+    document.getElementById(idItem).className="d-none"
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModal6'))
+
+
+}
+
+const exampleModal7 = document.getElementById('exampleModal7')
+exampleModal7.addEventListener('show.bs.modal', event => {
+  const button = event.relatedTarget
+  const recipient = button.getAttribute('data-bs-whatever')
+  const modalBodyInput = exampleModal7.querySelector('.modal-body input')
+  modalBodyInput.value = recipient
+})
+function  eliminarItem7(idItem) {
+    console.log(idItem)
+    console.log(document.getElementById(idItem))
+    document.getElementById(idItem).className="d-none"
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModal7'))
 
 
 }
